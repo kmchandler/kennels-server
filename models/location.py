c30a91cc175aded6d39861254974a5123084f887
Kristen Chandler <krismchandler@gmail.com>
2022-10-22 11:54:27 -0500

    cleared error chapter 9

--- models/location.py
+++ models/location.py
@@ -2,6 +2,7 @@ class Location():
     '''
     this is the docstring
     '''
-    def __init__(self, id, address):
+    def __init__(self, id, name, address):
         self.id = id
+        self.name = name
         self.address = address
